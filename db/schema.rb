ed85d52f2f120eacd3b7ac4efb3fe4289f1561da
Michael Pope <mpope.cr@gmail.com>
2015-03-31 15:24:02 -0400

    Implement ToDo model w/ description validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150331192208) do
+
+  create_table "to_dos", force: :cascade do |t|
+    t.string "description"
+  end
 
 end
