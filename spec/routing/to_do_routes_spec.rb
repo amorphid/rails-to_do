8458c3b02cb1d7ef81cd9f40c4352c1ec158b881
Michael Pope <mpope.cr@gmail.com>
2015-03-31 16:43:54 -0400

    Add route test for deleting a ToDo

--- spec/routing/to_do_routes_spec.rb
+++ spec/routing/to_do_routes_spec.rb
@@ -6,6 +6,13 @@ describe ToDosController do
   end
 
   it do
+    expect(delete: "/to_dos/:id").to route_to(
+      controller: "to_dos",
+      id: ":id"
+    )
+  end
+
+  it do
     expect(post: "/to_dos").to route_to("to_dos#create")
   end
 end
