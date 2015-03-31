679b1f99c3f2223544824bb14653a3282cbb87cf
Michael Pope <mpope.cr@gmail.com>
2015-03-31 15:37:54 -0400

    Add test for to_dos#create

--- spec/controllers/to_dos_controller_spec.rb
+++ spec/controllers/to_dos_controller_spec.rb
@@ -1,6 +1,14 @@
 require "rails_helper"
 
 describe ToDosController do
+  context "#create" do
+    it "creates a ToDo w/ valid input" do
+      pre_count = ToDo.count
+      post :create, to_do: Fabricate.build(:to_do)
+      expect(ToDo.count).to eq(pre_count + 1)
+    end
+  end
+
   context "#index" do
     it "sets @to_dos" do
       to_dos = Fabricate.times(2, :to_do)
